ec139703190b59e50917bd92518e4c3b77b715de
Ignacio <ignaciog1459@outlook.com>
2026-02-06 10:05:02 -0300

    Initial commit: Reserva de canchas

---
+++ src/main/java/com/example/reserva_canchas/domain/Field.java
@@ -0,0 +1,4 @@
+package com.example.reserva_canchas.domain;
+
+public class Field {
+}
---
+++ src/test/java/com/example/reserva_canchas/ReservaCanchasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reserva_canchas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaCanchasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
