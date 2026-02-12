df06bf7a0e8090cb385cd3a5aeeb023272bc86f2
Ignacio <ignaciog1459@outlook.com>
2026-02-12 17:04:14 -0300

    feat: crear DTOs de Reservation (request y response)

--- src/main/java/com/example/reserva_canchas/infrastructure/dto/request/CreateReservationRequestDTO.java
+++ src/main/java/com/example/reserva_canchas/infrastructure/dto/request/CreateReservationRequestDTO.java
@@ -1,13 +1,20 @@
-package com.example.reserva_canchas.infrastructure.dto;
+package com.example.reserva_canchas.infrastructure.dto.request;
 
+import com.example.reserva_canchas.domain.model.enums.ReservationStatus;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record CreateReservationRequestDTO(
 
     Long userId,
     Long fieldId,
     LocalDate date,
-
+    LocalTime startTime,
+    LocalTime endTime,
+    BigDecimal priceTotal
 
 ) {
 }
--- src/main/java/com/example/reserva_canchas/infrastructure/dto/response/ReservationResponseDTO.java
+++ src/main/java/com/example/reserva_canchas/infrastructure/dto/response/ReservationResponseDTO.java
@@ -1,4 +1,18 @@
 package com.example.reserva_canchas.infrastructure.dto.response;
 
-public record ReservationResponseDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationResponseDTO(
+
+        String name,
+        String fieldName,
+        String typeField,
+        LocalTime startTime,
+        LocalDate date,
+        BigDecimal price
+
+
+) {
 }
