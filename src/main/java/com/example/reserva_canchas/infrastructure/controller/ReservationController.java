030baf3e7fcf5e923f35cc4e98fea7a08dd7529e
Ignacio <ignaciog1459@outlook.com>
2026-02-17 23:18:25 -0300

    feat(reservation): finalizar implementación de endpoints REST para crear, listar y cancelar reservas

--- src/main/java/com/example/reserva_canchas/infrastructure/controller/ReservationController.java
+++ src/main/java/com/example/reserva_canchas/infrastructure/controller/ReservationController.java
@@ -9,12 +9,13 @@ import com.example.reserva_canchas.infrastructure.dto.response.ReservationRespon
 import com.example.reserva_canchas.infrastructure.mapper.ReservationMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
@@ -40,6 +41,67 @@ public class ReservationController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ReservationMapper.toResponse(reservation));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponseDTO>> getAllReservations() {
+
+        List<ReservationResponseDTO> reservations = getReservation.getReservations()
+                .stream()
+                .map(ReservationMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(reservations);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservationResponseDTO> getReservationById(@PathVariable Long id) {
+        Reservation reservation = getReservation.getReservationById(id);
+        return ResponseEntity.ok(ReservationMapper.toResponse(reservation));
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<ReservationResponseDTO>> getReservationByUserId(@Valid @PathVariable Long id) {
+
+        List<ReservationResponseDTO> reservations = getReservation.getReservationsByUserId(id)
+                .stream()
+                .map(ReservationMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(reservations);
+    }
+
+    @GetMapping("/date/{date}")
+    public ResponseEntity<List<ReservationResponseDTO>> getReservationsByDate(
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+            ) {
+
+        List<ReservationResponseDTO> reservations = getReservation.getReservationsByDate(date)
+                .stream()
+                .map(ReservationMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(reservations);
+    }
+
+    @GetMapping("/field/{id}")
+    public ResponseEntity<List<ReservationResponseDTO>> getReservationsByFieldAndDate(
+            @PathVariable Long fieldId,
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ){
+        List<ReservationResponseDTO> reservations = getReservation.getReservationsByFieldAndDate(fieldId, date).stream()
+                .map(ReservationMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(reservations);
+
+    }
+
+
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancelReservation(@PathVariable Long id){
+        cancelReservation.cancelReservation(id);
+        return ResponseEntity.ok().build();
     }
 
 
